0c0075ff39281fc90352c33c2d1390d7ad6ffcb2
julieta <julieta.lopez@darwoft.com>
2025-02-07 16:39:36 -0300

    Se agrega estilos

--- funciones.js
+++ funciones.js
@@ -1,10 +1,10 @@
-function cargarProyectos() {
+function cargarProyectos(proyectosAmostrar = proyectos) {
     console.log(proyectos);
 
     const container = document.getElementsByClassName("grid-container-proyectos")[0];
     let contenido = "";
 
-    proyectos.forEach((element, $id) => {
+    proyectosAmostrar.forEach((element, $id) => {
         contenido += `<div class="proyectos" onClick="displayModal(${$id})">
             <img src="images/${element.imagen}" alt="${element.proyecto}" width="200" height="270" />
             <h5>${element.proyecto}</h5>
@@ -14,11 +14,11 @@ function cargarProyectos() {
     container.innerHTML = contenido;
 }
 
-function displayModal(numberCert) {
+function displayModal(numberProyecto) {
     const modal = document.getElementById("proyectos-modal");
     modal.style.display = "block";
 
-    const element = proyectos[numberCert];
+    const element = proyectos[numberProyecto];
     modal.innerHTML = `<h5>${element.proyecto}</h5>
                         <img src="images/${element.imagen}" alt="${element.proyecto}" />
                         <p>${element["descripcion"]}</p>
@@ -30,21 +30,10 @@ function closeModal() {
 }
 
 /**Buscador */
-function buscarProyecto() {
-    let input = document.getElementById("search").value.toLowerCase();
-    console.log(input)
-    let proyectos = document.querySelectorAll(".proyecto");
-
-    proyectos.forEach(proyecto => {
-        let nombre = proyecto.getAttribute("proyectos").toLowerCase();
-
-        if (nombre.includes(input)) {
-            proyecto.style.display = "block"; // Muestra el proyecto si coincide
-        } else {
-            proyecto.style.display = "none"; // Oculta si no coincide
-        }
-    });
+function buscarProyecto(proyecto) {
+    const proyectosFiltrados = proyectos.filter((proyectos) =>
+        proyectos.proyecto.toLocaleLowerCase().includes(proyecto.toLocaleLowerCase())
+    );
+    console.log(JSON.stringify(proyectosFiltrados));
+    cargarProyectos(proyectosFiltrados);
 }
-
-// También detecta búsqueda al escribir en el input
-document.getElementById("search").addEventListener("keyup", buscarProyecto);
